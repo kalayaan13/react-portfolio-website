7d3832228cfd3a538a5fb3dacd5f54a5af45e96d
Kalcent Rabbi Santiago <kalcentsantiago@gmail.com>
2022-08-10 17:48:36 +0800

    Test

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,7 +3,14 @@ import './header.css'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header>
+      <div className="container header__container">
+          <h5>Hello I'm</h5>
+          <h1>Kalcent Rabbi Santiago</h1>
+          <h5 className="text-light"> Fullstack Developer</h5>
+          
+      </div>
+    </header>
   )
 }
 
